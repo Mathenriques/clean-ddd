83ac4100f22f10ad6ba05c2ed727ed62c70eecda
Matheus Henriques <math.marqui@gmail.com>
2023-08-28 20:30:38 -0300

    Alterando estruturação para Clean Architecture

--- tests/unit/value-objects/slug.spec.ts
+++ tests/unit/value-objects/slug.spec.ts
@@ -1,5 +1,5 @@
-import { Slug } from "@/domain/entities/value-objects/slug"
-import { expect, test } from "vitest"
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
+import { expect, test } from 'vitest'
 
 test('Should be able to create a new slug from text', () => {
   const slug = Slug.createFromText('Example question title')
