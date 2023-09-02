8cce476157cbd5c96e131196ffd672604249b3d7
Matheus Henriques <math.marqui@gmail.com>
2023-09-02 15:35:40 -0300

    Adicionando dados fake

--- tests/factories/make-question.ts
+++ tests/factories/make-question.ts
@@ -10,12 +10,15 @@ export function makeQuestion(
   override: Partial<QuestionProps> = {},
   id?: UniqueEntityID,
 ) {
-  const question = Question.create({
-    authorId: new UniqueEntityID(),
-    title: faker.lorem.sentence(),
-    content: faker.lorem.text(),
-    ...override,
-  })
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityID(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
